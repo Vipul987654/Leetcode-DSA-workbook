687e88ce344eb7362db2da261049c9785d627ba9
Vipul987654 <singhvipulpratap2004@gmail.com>
2025-04-20 00:28:13 +0530

    Time: 6 ms (27.42%), Space: 41.7 MB (71.35%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,34 +1,27 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Solution {
-    public List<String> summaryRanges(int[] nums) {
-        int n = nums.length;
-        List<String> res = new ArrayList<>();
-
-        if (n == 0) {
-            return res;
-        }
-
-        for (int i = 0; i < n; ) {
-            int start = nums[i];
-            int end = nums[i];
-
-            // Find the end of the current consecutive range
-            while (i + 1 < n && nums[i + 1] == end + 1) {
-                end++;
-                i++;
-            }
-
-            // Add the range to the result list
-            if (start == end) {
-                res.add(String.valueOf(start));
-            } else {
-                res.add(start + "->" + end);
-            }
-            i++; // Move to the next potential start of a range
-        }
-
-        return res;
+class Solution 
+{
+    public List<String> summaryRanges(int[] nums) 
+    {   int n=nums.length;
+       int count =0;
+      List<String> res=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        {
+            int start=nums[i];
+            while(i+1<n && nums[i]+1==nums[i+1])
+            {
+            i++;
+             }
+           if(start!=nums[i])
+           {
+           // res.add(toString(start)+"->"+toString(nums[i]));
+           res.add(start + "->" + nums[i]);
+              }
+        else
+            {
+           // res.add(toString(start));
+           res.add(String.valueOf(start));
+          
+             }}
+      return res;
     }
 }
