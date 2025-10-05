225f59d2c54f6a84e4e5ea36d97b20e93f596dd5
Vipul987654 <singhvipulpratap2004@gmail.com>
2025-10-05 15:57:37 +0530

    Time: 0 ms (100%), Space: 44.5 MB (71.18%) - LeetHub

---
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -0,0 +1,24 @@
+class Solution {
+    TreeNode prev = null;
+    int minDiff = Integer.MAX_VALUE;
+
+    public void helper(TreeNode root) {
+        if (root == null)
+            return;
+
+        helper(root.left);
+
+        if (prev != null) {
+            minDiff = Math.min(minDiff, Math.abs(root.val - prev.val));
+        }
+
+        prev = root;
+
+        helper(root.right);
+    }
+
+    public int getMinimumDifference(TreeNode root) {
+        helper(root);
+        return minDiff;
+    }
+}
