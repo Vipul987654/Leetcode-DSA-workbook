859d5b7b835b240debd94e7187ecaffbb72984be
VIPUL PRATAP SINGH <singhvipulpratap2004@gmail.com>
2026-03-09 15:29:21 +0530

    Time: 6 ms (69.18%), Space: 45.7 MB (68.08%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,23 +1,19 @@
-class Solution 
-{
-    public int lengthOfLongestSubstring(String s) 
-    {
-        Set<Character>set =new HashSet<>();
-        int maxLen=0;
-        int left=0;
-
-        for(int right=0;right<s.length();right++)
+class Solution {
+    public int lengthOfLongestSubstring(String s)
+     {
+        int maxLength=0;
+        Map<Character,Integer> visitedCharacters=new HashMap<>();
+        for(int right = 0, left = 0 ; right < s.length() ; right++)
         {
-            while(set.contains(s.charAt(right)))
+            if(visitedCharacters.containsKey(s.charAt(right))   // checking if the map contains the character already or not;
+            && visitedCharacters.get(s.charAt(right))>=left)   //checking if the duplicate char is in the  sliding window range;
+                                                               
             {
-                set.remove(s.charAt(left));
-                left++;
+                left=visitedCharacters.get(s.charAt(right))+1;  // shifting the left pointer to one more next to the previously occured character ;
             }
-
-            set.add(s.charAt(right));
-
-            maxLen=Math.max(maxLen,right-left+1);
+            maxLength = Math.max(maxLength,right-left+1);  // calc max 
+            visitedCharacters.put(s.charAt(right),right);  // update the latest index of occurence  of the character;
         }
-        return maxLen;
+        return maxLength;
     }
 }
